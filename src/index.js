cb0f64e093faae2859416e50556c02a2394f1c52
jbockerstette <jim.bockerstette@gmail.com>
2017-09-05 13:44:46 -0700

    Sass is now compiled to css. Added added bootstrap-sass so that we have the needed sass variables.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import css from './scss/app.scss';
 
 import React from 'react';
 import ReactDom from 'react-dom';
--- src/js/Collapsible.js
+++ src/js/Collapsible.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+//Required or nothing will be styled.
+import css from '../scss/app.css';
+
+
 
 class Collapsible extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false,
+    };
+  }
+
+  handleToggle(e) {
+    e.preventDefault();
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    });
+  }
+
   render() {
     const {title, children} = this.props;
-
+    const {isExpanded} = this.state;
     return (
-      <div className="panel">
+      <div className={`panel ${isExpanded ? "is-expanded":"" }`} onClick={(e) => this.handleToggle(e)}>
         <div className="panel-heading">
           <h2>{title}</h2>
         </div>
