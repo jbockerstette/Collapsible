bac29e40e3582dbcef6584b453d927916f93551d
jbockerstette <jim.bockerstette@gmail.com>
2017-09-05 13:59:30 -0700

    Completed.

--- src/js/Collapsible.js
+++ src/js/Collapsible.js
@@ -15,21 +15,24 @@ class Collapsible extends React.Component {
 
   handleToggle(e) {
     e.preventDefault();
+    console.log(this.refs.inner.clientHeight);
     this.setState({
-      isExpanded: !this.state.isExpanded
+      isExpanded: !this.state.isExpanded,
+      height: this.refs.inner.clientHeight
     });
   }
 
   render() {
     const {title, children} = this.props;
-    const {isExpanded} = this.state;
+    const {isExpanded, height} = this.state;
+    const currentHeight = isExpanded ? height : 0;
     return (
       <div className={`panel ${isExpanded ? "is-expanded":"" }`} onClick={(e) => this.handleToggle(e)}>
         <div className="panel-heading">
           <h2>{title}</h2>
         </div>
-        <div className="panel-collapse">
-          <div className="panel-body">
+        <div className="panel-collapse" style={{height: currentHeight+'px'}}>
+          <div className="panel-body" ref="inner">
             {children}
           </div>
         </div>
